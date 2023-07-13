e0ef05a37d4db1a6340f5336851e191a362b1f23
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-13 02:29:37 +0200

    Rute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./komponente/navbar";
+import { Prodavnica } from "./stranice/prodavnica/prodavnica";
+import { Korpa } from "./stranice/korpa/korpa";
+import { ShopContextProvider } from "./context/shop-context";
+import { Footer } from "./komponente/footer";
 
 function App() {
   return (
     <div className="App">
-      
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Prodavnica />} />
+            <Route path="/cart" element={<Korpa />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
