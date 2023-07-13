3adfb77bba330ccb42e719570a587a80941cfbda
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-13 02:31:13 +0200

    Lista proizvoda

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import p1 from "./img/umivalica.webp";
+import p2 from "./img/krema1.webp";
+import p3 from "./img/micVoda.webp";
+import p4 from "./img/piling.webp";
+import p5 from "./img/maska.webp";
+import p6 from "./img/kremaTelo.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    nazivPr: "Gel za čišćenje lica",
+    cena: 2000.0,
+    slikaPr: p1,
+  },
+  {
+    id: 2,
+    nazivPr: "Kremica",
+    cena: 1500.0,
+    slikaPr: p2,
+  },
+  {
+    id: 3,
+    nazivPr: "Micelarna voda",
+    cena: 600.0,
+    slikaPr: p3,
+  },
+  {
+    id: 4,
+    nazivPr: "Piling za lice",
+    cena: 300.0,
+    slikaPr: p4,
+  },
+  {
+    id: 5,
+    nazivPr: "Maska za lice",
+    cena: 150.0,
+    slikaPr: p5,
+  },
+  {
+    id: 6,
+    nazivPr: "Krema za telo",
+    cena: 2100.0,
+    slikaPr: p6,
+  },
+];
