c1bc67e4cd13819f88ce98eb2163af54d0e89809
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-13 02:32:11 +0200

    futer

---
+++ src/komponente/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div
+      style={{ backgroundColor: "#7bb074", padding: "60px", color: "white" }}
+    >
+      <span style={{ float: "left", fontSize: "40px", color: "black" }}>
+        <b> Dobro dosli u prodavnicu, uzivajte u kupovini!</b>
+      </span>
+    </div>
+  );
+};
